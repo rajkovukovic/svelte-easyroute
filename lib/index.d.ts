2890e892bec2ad8949de6c16c5fdadf0b4694571
Rajko Vukovic <rajkovukovic@gmail.com>
2021-01-31 09:21:33 +0100

    add types all:  any

---
+++ lib/index.d.ts
@@ -0,0 +1,10 @@
+declare module 'svelte-easyroute' {
+  /**
+   * Data to define route
+   */
+  export type Router = any
+  export type RouterLink = any
+  export type EasyrouteProvider = any
+  export type RouterOutlet = any
+  export type useCurrentRoute = any
+}
